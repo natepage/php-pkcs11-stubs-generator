1500123a71cca351841d10dd007301fb6ed18c0f
nathanpage <nathan.page@eonx.com>
2023-06-08 22:21:02 +1000

    Try to fix paths in php

--- bin/generate-stubs.php
+++ bin/generate-stubs.php
@@ -6,7 +6,7 @@ use NatePage\PhpPkcs11\StubsGenerator\StubsGenerator;
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
-$distDir = __DIR__ . '/../../repos/php-pkcs11-ide-helper';
-$extensionDir = __DIR__ . '/../../repos/php-pkcs11';
+$distDir = __DIR__ . '/../../../repos/php-pkcs11-ide-helper';
+$extensionDir = __DIR__ . '/../../../repos/php-pkcs11';
 
 (new StubsGenerator())->generate($distDir, $extensionDir);
