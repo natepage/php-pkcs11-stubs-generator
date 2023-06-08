ee106121c8ea5ea13ca238a204411cf9e6ea2ea7
nathanpage <nathan.page@eonx.com>
2023-06-08 22:55:03 +1000

    Update dist dir to generate in src

--- bin/generate-stubs.php
+++ bin/generate-stubs.php
@@ -6,7 +6,7 @@ use NatePage\PhpPkcs11\StubsGenerator\StubsGenerator;
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
-$distDir = __DIR__ . '/../../../repos/php-pkcs11-ide-helper';
+$distDir = __DIR__ . '/../../../repos/php-pkcs11-ide-helper/src';
 $extensionDir = __DIR__ . '/../../../repos/php-pkcs11';
 
 (new StubsGenerator())->generate($distDir, $extensionDir);
